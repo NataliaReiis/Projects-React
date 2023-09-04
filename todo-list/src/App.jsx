82cb687b85636f74324db5992017db831751633d
NataliaReiis <nataliareis208@gmail.com>
2023-09-04 03:41:12 -0300

    projeto todo list em reactjs

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import './App.css';
+import Todo from './components/Todo';
+import TodoForm  from './components/TodoForm';
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar Layout responsivo",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Comprar Creatina",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar Calculo 3",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ])
+
+  const addTodo = (text, category) => {
+    const newTodos = [... todos,
+      {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false
+      },
+    ];
+
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter(todo => todo.id !== id ? todo: null);
+    setTodos(filteredTodos)
+  };
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) => todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo);
+    setTodos(newTodos);
+  };
+
+  return <div className='app'>
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
+        ))}
+      </div>
+        <TodoForm addTodo={addTodo} />
+    </div>
+}
+
+export default App
